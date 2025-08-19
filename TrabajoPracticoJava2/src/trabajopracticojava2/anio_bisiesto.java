29b852c148e5199482d58ebffc1f2045c531f3e6
MaxiCode <andresholsbach@gmail.com>
2025-08-18 23:21:28 -0300

    SE AGREGA EJERCICIO 1

--- TrabajoPracticoJava2/src/trabajopracticojava2/anio_bisiesto.java
+++ TrabajoPracticoJava2/src/trabajopracticojava2/anio_bisiesto.java
@@ -1,10 +1,26 @@
 package trabajopracticojava2;
-
+import java.util.Scanner;
 /**
  *
  * @author Maxi
+ * Verificación de Año Bisiesto.
+ * Escribe un programa en Java que solicite al usuario un año y determine si es bisiesto.
+ * Un año es bisiesto si es divisible por 4, pero no por 100, salvo que sea divisible por 400.
  * 
  */
 public class anio_bisiesto {
-    
+    public static void es_bisiesto(int anio){
+        if(anio % 4 == 0 && (anio % 100 != 0 || anio % 400 == 0)){
+            System.out.println("El año ingresado "+ anio + " es bisiesto.");
+        }else{
+            System.out.println("El año ingresado "+ anio + " no es bisiesto.");
+        }
+    }
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        int anio;
+        System.out.print("Ingrese el año para saber si es bisiesto o no: ");
+        anio = Integer.parseInt(input.nextLine());
+        es_bisiesto(anio);
+    }
 }
